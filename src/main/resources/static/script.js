f77e920e3e55ed247d90fe57af5f3978e4c8c28d
Natã Paulino Souza Santos <natapaulinoss@MacBook-Air-de-Nata.local>
2025-11-17 22:35:02 -0300

    versin

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://safe-transfer-api-xxxxx.onrender.com';
+const API_BASE = 'https://safe-transfer-api-1234.onrender.com';
 
 function $(s) { return document.querySelector(s); }
 

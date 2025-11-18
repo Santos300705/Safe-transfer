33212ae1906c3ad85504f2d7288202ddd5b5ee6e
Natã Paulino Souza Santos <natapaulinoss@MacBook-Air-de-Nata.local>
2025-11-18 12:58:32 -0300

    aaa

--- src/main/java/com/safetransfer/safertransfer/CorsConfig/CorsConfig.java
+++ src/main/java/com/safetransfer/safertransfer/CorsConfig/CorsConfig.java
@@ -1,29 +1,20 @@
 package com.safetransfer.safertransfer.CorsConfig;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "https://safe-transfer-1234.onrender.com", // seu front
-                                "http://localhost:5500",
-                                "http://127.0.0.1:5500"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(false);
-            }
-        };
-    }
+public class CorsConfig implements WebMvcConfigurer {
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+registry.addMapping("/api/**")
+.allowedOrigins(
+"https://produtos-frontend-o38v.onrender.com",
+"http://localhost:8080",
+"http://localhost:3000"
+)
+.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+.allowedHeaders("*")
+.allowCredentials(true);
+}
 }

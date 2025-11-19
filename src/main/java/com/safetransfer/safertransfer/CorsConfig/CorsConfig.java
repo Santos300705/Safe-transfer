6e78beb871fa6e7ff5f9ca3ec8f364d7452506bf
Natã Paulino Souza Santos <natapaulinoss@MacBook-Air-de-Nata.local>
2025-11-18 22:42:38 -0300

    vasco

--- src/main/java/com/safetransfer/safertransfer/CorsConfig/CorsConfig.java
+++ src/main/java/com/safetransfer/safertransfer/CorsConfig/CorsConfig.java
@@ -1,20 +1,28 @@
 package com.safetransfer.safertransfer.CorsConfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-@Override
-public void addCorsMappings(CorsRegistry registry) {
-registry.addMapping("/api/**")
-.allowedOrigins(
-"https://front-lqki.onrender.com",
-"http://localhost:8080",
-"http://localhost:3000"
-)
-.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-.allowedHeaders("*")
-.allowCredentials(true);
-}
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "https://front-lqki.onrender.com",
+                                "http://localhost:8080",
+                                "http://localhost:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
 }
